8299d28a9a6554b0483eb03369f3f8e8e10a62cf
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2024-01-11 18:33:25 +1030

    Level 2: Maze Basics

--- pacman/nodes.py
+++ pacman/nodes.py
@@ -1,6 +1,7 @@
 import pygame
 from vector import Vector2
 from constants import *
+import numpy as np
 
 class Node(object):
     def __init__(self, x, y):
@@ -19,43 +20,75 @@ class Node(object):
                 pygame.draw.circle(screen, RED, self.position.asInt(), 12)
 
 class NodeGroup(object):
-    def __init__(self):
-        self.nodeList = []
-
-    def setupTestNodes(self):
-        nodeA = Node(80, 80)
-        nodeB = Node(160, 80)
-        nodeC = Node(80, 160)
-        nodeD = Node(160, 160)
-        nodeE = Node(208, 160)
-        nodeF = Node(80, 320)
-        nodeG = Node(208, 320)
-        nodeA.neighbors[RIGHT] = nodeB
-        nodeA.neighbors[DOWN] = nodeC
-        nodeB.neighbors[LEFT] = nodeA
-        nodeB.neighbors[DOWN] = nodeD
-        nodeC.neighbors[UP] = nodeA
-        nodeC.neighbors[RIGHT] = nodeD
-        nodeC.neighbors[DOWN] = nodeF
-        nodeD.neighbors[UP] = nodeB
-        nodeD.neighbors[LEFT] = nodeC
-        nodeD.neighbors[RIGHT] = nodeE
-        nodeE.neighbors[LEFT] = nodeD
-        nodeE.neighbors[DOWN] = nodeG
-        nodeF.neighbors[UP] = nodeC
-        nodeF.neighbors[RIGHT] = nodeG
-        nodeG.neighbors[UP] = nodeE
-        nodeG.neighbors[LEFT] = nodeF
-
-        self.nodeList = [ nodeA,
-                          nodeB,
-                          nodeC,
-                          nodeD,
-                          nodeE,
-                          nodeF,
-                          nodeG ]
+    def __init__(self, level):
+        self.level = level
+        self.nodesLUT = {}
+        self.nodeSymbols = ['+']
+        self.pathSymbols = ['.']
+        data = self.readMazeFile(level)
+        self.createNodeTable(data)
+        self.connectHorizontally(data)
+        self.connectVertically(data)
 
     def render(self, screen):
-        for node in self.nodeList:
+        for node in self.nodesLUT.values():
             node.render(screen)
 
+    def readMazeFile(self, textfile):
+        return np.loadtxt(textfile, dtype='<U1')
+
+    def createNodeTable(self, data, xoffset=0, yoffset=0):
+        for row in list(range(data.shape[0])):
+            for col in list(range(data.shape[1])):
+                if data[row][col] in self.nodeSymbols:
+                    x,y = self.constructKey(col+xoffset, row+yoffset)
+                    self.nodesLUT[(x,y)] = Node(x, y)
+
+    def constructKey(self, x, y):
+        return x*TILEWIDTH, y*TILEHEIGHT
+
+    def connectHorizontally(self, data, xoffset=0, yoffset=0):
+        for row in list(range(data.shape[0])):
+            key = None
+            for col in list(range(data.shape[1])):
+                if data[row][col] in self.nodeSymbols:
+                    if key is None:
+                        key = self.constructKey(col+xoffset, row+yoffset)
+                    else:
+                        otherkey = self.constructKey(col+xoffset, row+yoffset)
+                        self.nodesLUT[key].neighbors[RIGHT] = self.nodesLUT[otherkey]
+                        self.nodesLUT[otherkey].neighbors[LEFT] = self.nodesLUT[key]
+                        key = otherkey
+                elif data[row][col] not in self.pathSymbols:
+                    key = None
+
+    def connectVertically(self, data, xoffset=0, yoffset=0):
+        dataT = data.transpose()
+        for col in list(range(dataT.shape[0])):
+            key = None
+            for row in list(range(dataT.shape[1])):
+                if dataT[col][row] in self.nodeSymbols:
+                    if key is None:
+                        key = self.constructKey(col+xoffset, row+yoffset)
+                    else:
+                        otherkey = self.constructKey(col+xoffset, row+yoffset)
+                        self.nodesLUT[key].neighbors[DOWN] = self.nodesLUT[otherkey]
+                        self.nodesLUT[otherkey].neighbors[UP] = self.nodesLUT[key]
+                        key = otherkey
+                elif dataT[col][row] not in self.pathSymbols:
+                    key = None
+
+    def getNodesFromPixels(self, xpixel, ypixel):
+        return self.nodesLUT[(xpixel, ypixel)] \
+            if (xpixel, ypixel) in self.nodesLUT.keys()\
+               else None
+
+    def getNodeFromTiles(self, col, row):
+        x,y = self.constructKey(col, row)
+        return self.nodesLUT[(x, y)] \
+            if (x, y) in self.nodesLUT.keys() \
+               else None
+
+    def getStartTempNode(self):
+        nodes = list(self.nodesLUT.values())
+        return nodes[0]
--- pacman/run.py
+++ pacman/run.py
@@ -17,9 +17,8 @@ class GameController(object):
 
     def startGame(self):
         self.setBackground()
-        self.nodes = NodeGroup()
-        self.nodes.setupTestNodes()
-        self.pacman = Pacman(self.nodes.nodeList[0])
+        self.nodes = NodeGroup('mazetest.txt')
+        self.pacman = Pacman(self.nodes.getStartTempNode())
 
     def update(self):
         dt = self.clock.tick(30) / 1000.0
