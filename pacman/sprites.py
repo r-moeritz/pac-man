970d7200f252b25078c9259f4b56e39e41aaafb4
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2024-04-26 18:23:08 +0930

    Fix transparency issue

--- pacman/sprites.py
+++ pacman/sprites.py
@@ -11,6 +11,8 @@ class Spritesheet(object):
 
     def __init__(self):
         self.sheet = pygame.image.load('spritesheet.png').convert()
+        transcolor = self.sheet.get_at((0, 0))
+        self.sheet.set_colorkey(transcolor)
         width = int(self.sheet.get_width() / BASETILEWIDTH * TILEWIDTH)
         height = int(self.sheet.get_height() / BASETILEHEIGHT * TILEHEIGHT)
         self.sheet = pygame.transform.scale(self.sheet, (width, height))
